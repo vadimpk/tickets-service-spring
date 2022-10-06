597e434b35fbeea58602be04f370c0f82df46cb8
Stas <stbukovskiy12@gmail.com>
2022-10-06 16:50:54 +0300

    Fill Station class

--- src/main/java/com/naukma/ticketsservice/station/Station.java
+++ src/main/java/com/naukma/ticketsservice/station/Station.java
@@ -1,4 +1,34 @@
 package com.naukma.ticketsservice.station;
 
+import java.time.Duration;
+import java.util.Map;
+
 public class Station {
+
+    private Map<Station, Integer> adjacentStations;
+    private Duration duration;
+
+    public Station() {
+
+    }
+
+    public void addAdjacentStation(Station station, int distance) {
+        adjacentStations.put(station, distance);
+    }
+
+    public Map<Station, Integer> getAdjacentStations() {
+        return adjacentStations;
+    }
+
+    public void setAdjacentStations(Map<Station, Integer> adjacentStations) {
+        this.adjacentStations = adjacentStations;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
 }
