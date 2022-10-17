b2bfcf59ce2adaec35ea421710ab0eecbe5420cf
Stas <stbukovskiy12@gmail.com>
2022-10-17 19:01:46 +0300

    Add logging functionality

--- src/main/java/com/naukma/ticketsservice/TicketsServiceApplication.java
+++ src/main/java/com/naukma/ticketsservice/TicketsServiceApplication.java
@@ -1,13 +1,21 @@
 package com.naukma.ticketsservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class TicketsServiceApplication {
 
+
+    static final Logger log =
+            LoggerFactory.getLogger(TicketsServiceApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(TicketsServiceApplication.class, args);
+        log.debug("Starting TicketsServiceApplication application in debug with {} args", args.length);
+        log.info("Starting TicketsServiceApplication application with {} args.", args.length);
     }
 
 }
