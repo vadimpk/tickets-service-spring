60eaae900c4632e4827166506f1d557d93022cf6
jester_69m <pashchuknazar@ukr.net>
2022-11-07 21:08:12 +0200

    add validation

--- src/main/java/com/naukma/ticketsservice/train/Wagon.java
+++ src/main/java/com/naukma/ticketsservice/train/Wagon.java
@@ -1,5 +1,11 @@
 package com.naukma.ticketsservice.train;
+import org.springframework.lang.NonNull;
+
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "wagons")
@@ -7,12 +13,17 @@ public class Wagon {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @NonNull
     private long id;
 
     @Column(unique = true, nullable = false)
+    @Pattern(regexp = "[a-zA-z0-9]{3,16}", message = "wagon name length must be of 3 to 16 element with no special parameter")
+//    @NotBlank
     private String name;
 
     @Column(nullable = false)
+    @Min(10)
+    @Max(200)
     private int numberOfSeats;
 
     @ManyToOne(cascade = CascadeType.ALL)
